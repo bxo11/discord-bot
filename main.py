598bfe2fe2ec9e3c10d26f6bfd0984aea59a033b
KrzysiekKlim <krzysiek.klim1999@gmail.com>
2021-04-13 22:06:57 +0200

    Added old rules to db, rewerited show_regulations method

--- main.py
+++ main.py
@@ -17,29 +17,43 @@ engine = db.load_database()
 Session = sessionmaker(bind=engine)
 session = Session()
 
+global rules_channel
+
 
 @bot.event
 async def on_ready():
+    global rules_channel
+    rules_channel = session.query(Configuration).filter(
+        Configuration.SettingName == 'RulesChannel').first().SettingValue
     print('Logged in as:')
     print('Username: ' + bot.user.name)
     print('------')
 
 
-@bot.command(name='radd', help='Adding new rules')
+@bot.command(name='radd', help='Adding new rule')
 async def rule_add(context, mess):
+    global rules_channel
+    if rules_channel != context.channel.name:
+        print("Wrong channel")
+        return
+
     current_position = session.query(Configuration).filter(Configuration.SettingName == 'CurrentRulePosition').first()
     rule = Rules(mess, context.message.author.name + '#' + context.message.author.discriminator,
                  current_position.SettingValue)
     session.add(rule)
     current_position.SettingValue = int(current_position.SettingValue) + 1
     session.commit()
-    response = "Rule added"
-    await context.send(response)
+    print("Rule added")
     await show_regulations(context)
 
 
-@bot.command(name='rdel', help='Deleting new rules')
+@bot.command(name='rdel', help='')
 async def rule_delete(context, position):
+    global rules_channel
+    if rules_channel != context.channel.name:
+        print("Wrong channel")
+        return
+
     current_position = session.query(Configuration).filter(Configuration.SettingName == 'CurrentRulePosition').first()
     rule_to_delete = session.query(Rules).filter(Rules.Position == position).first()
 
@@ -50,7 +64,7 @@ async def rule_delete(context, position):
     session.delete(rule_to_delete)
     current_position.SettingValue = int(current_position.SettingValue) - 1
     session.commit()
-    await context.send('Rule deleted')
+    print('Rule deleted')
     recalculate_positions()
     await show_regulations(context)
 
@@ -69,15 +83,24 @@ def recalculate_positions():
 
 @bot.command(name='rshow', help='')
 async def show_regulations(context):
-    embed = discord.Embed(title="Regulamin")
-    list_of_rules = session.query(Rules).all()
-    i = 1
-    for elem in list_of_rules:
-        text = str(i) + ". " + elem.Text
-        embed.add_field(name='\u200b', value=text, inline=False)
-        i += 1
+    global rules_channel
+    if rules_channel != context.channel.name:
+        print("Wrong channel")
+        return
 
-    await context.send(embed=embed)
+    await context.channel.purge(limit=10)
+    list_of_rules = session.query(Rules).all()
+
+    i = 1
+    while i < len(list_of_rules):
+        embed = discord.Embed(title="Regulamin")
+        for elem in list_of_rules[i - 1:]:
+            text = str(elem.Position) + ". " + elem.Text
+            embed.add_field(name='\u200b', value=text, inline=False)
+            i += 1
+            if i % 26 == 0:
+                break
+        await context.send(embed=embed)
 
 
 bot.run(TOKEN)
