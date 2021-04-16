7fc27c5593dd963c3a47b389d8028d69fe9a8081
KrzysiekKlim <krzysiek.klim1999@gmail.com>
2021-04-17 01:13:34 +0200

    Fixed heroku postgresql error

--- db.py
+++ db.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 def load_database():
     engine = create_engine(DATABASE_URL, echo=True)
